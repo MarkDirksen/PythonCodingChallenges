f0da6be4579612e5802593eec73fb8fafe8e5213
Mark Dirksen <mark.dirksen@visma.com>
2019-08-14 20:07:02 +0200

    LawOfLargeNumbers.py

--- LawOfLargeNumbers.py
+++ LawOfLargeNumbers.py
@@ -1,10 +1,19 @@
 import math
 import numpy as np
+#import matplotlib.pyplot as plt
 from numpy.random import randn
 
-msg = "Waddup"
-print(msg)
-print(math.sqrt(4))
+n = 100000
+m = 0
+l = []
 
-x=randn()
-print(x)
+for i in range(n):
+    x=randn()
+    if (x > -1) and (x <= 1):
+        m = m + 1
+    l.append(m/(i+1))
+
+#print(l)
+
+#plt.scatter(range(n),l)
+#plt.show()
